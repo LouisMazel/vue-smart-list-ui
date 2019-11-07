f23eeda8db54f8c7a54453f99b2da7019befb6b6
Mazel <mazuel.loic@gmail.com>
2019-11-07 19:14:04 +0100

    (fix) publicPath in vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,9 @@
 const path = require('path')
 
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? './'
+    : '/',
   pluginOptions: {
     'style-resources-loader': {
       preProcessor: 'scss',
